bbd393ceced24731cc23e633991620f8973a7c6e
David Glenn Guillory Jr <dguillory62888@gmail.com>
2021-01-29 19:22:30 -0600

    Webpack completed

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const srcPath = path.resolve(__dirname, 'client', 'src');
+const distPath = path.resolve(__dirname, 'client', 'dist');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  watch: true,
+  entry: path.resolve(srcPath, 'index.jsx'),
+  output: {
+    path: distPath,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
