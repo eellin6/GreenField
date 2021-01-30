593a5c0dce93f624d26737b8e4f22852932b28fe
Austin Vollman <austinvollman@Austins-MacBook-Pro.local>
2021-01-30 11:04:38 -0600

    working on database

--- server/db/database.js
+++ server/db/database.js
@@ -1,2 +1,20 @@
 const { Sequelize } = require('sequelize');
-const db = new Sequelize('chatter', 'root', '');
+const db = new Sequelize('Maps', 'root', '');
+
+const User = db.define('User', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+});
+
+const Markers = db.define('Markers', {
+  latitude: Sequelize.INTEGER,
+  longitude: Sequelize.INTEGER,
+  imageUrl: Sequelize.STRING,
+
+});
+const Favorites = db.define('Markers', {
+  latitude: Sequelize.INTEGER,
+  longitude: Sequelize.INTEGER,
+  imageUrl: Sequelize.STRING,
+
+});
