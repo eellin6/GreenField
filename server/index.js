86e6846bd8b8e02d256482df7143bfed24fb4f33
David Glenn Guillory Jr <dguillory62888@gmail.com>
2021-02-01 10:25:16 -0600

    created routes to login and register page

--- server/index.js
+++ server/index.js
@@ -6,6 +6,24 @@ const axios   = require('axios');
 const bodyParser= require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '..','client','dist')))
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) => {
+  //render homepage?
+  res.render('index.html')
+
+})
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+})
+
+
+
+
 app.listen(3000, function() {
   console.log('listening on 3000')
 })
