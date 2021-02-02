d97ccc3fc5e413d2d0c2ddbfe8e43a3d2c5d5110
David Glenn Guillory Jr <dguillory62888@gmail.com>
2021-02-01 18:51:48 -0600

    Dinner break

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import GoogleApiWrapper from './Map';
 import MapContainer from './Map'
 import axios from 'axios'
-
+import login from './Login'
+import register from './Register'
+import Top10 from './Top10'
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      seen: false
     }
   }
 render() {
   return (
+    <div>
+<h1>HELLO WORLD</h1>
 <div>
   <GoogleApiWrapper />
+
 </div>
+    </div>
   )
 }
 }
