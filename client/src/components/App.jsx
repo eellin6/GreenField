a8946b9550de209d5cbdba817010d5bd210772b2
Austin Vollman <austinvollman@Austins-MacBook-Pro.local>
2021-02-02 13:41:05 -0600

    minimal changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import GoogleApiWrapper from './Map';
 import MapContainer from './Map'
 import axios from 'axios'
-<<<<<<< HEAD
-import Top10 from './Top10';
-=======
 import Login from './Login'
->>>>>>> cf18585504b6bbd045a2bdcd374fac5e52007fab
 
 import Top10 from './Top10'
 class App extends Component {
@@ -33,19 +29,11 @@ render() {
     <div>
 <button onClick={this.handleClick}>{status}</button>
 <div>
-<<<<<<< HEAD
-<div><GoogleApiWrapper /></div>
-
-<div><Top10/></div>
-</div>
-
-=======
 {status === 'Login' ? <Login /> :
   <GoogleApiWrapper />}
 
 </div>
     </div>
->>>>>>> cf18585504b6bbd045a2bdcd374fac5e52007fab
   )
 }
 }
