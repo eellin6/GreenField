8648673af9fb49bb6d820fd134b722e560fe80e6
Ian <iancoder@1097@gmail.com>
2021-02-09 16:38:32 -0600

    FIX/ eslint errors fixed

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -5,29 +5,30 @@ const { Comments } = require('../db/database');
 
 router.route('/').get((req, res) => {
 
-    return Comments.findAll({})
-        .then((data) => { res.send(data)})
-        .catch((err) => {console.log(err)
-        })
+  return Comments.findAll({})
+    .then((data) => { res.send(data); })
+    .catch((err) => {
+      console.log(err);
+    });
 
-})
+});
 
 
 router.route('/').post((req, res) => {
 
-  const { comments, description } = req.body
+  const { comments, description } = req.body;
 
-        const newComment = new Comments({
-          comments,
-          description
-        });
+  const newComment = new Comments({
+    comments,
+    description
+  });
 
-        newComment.save()
-          .then((data) => {
-            console.log('COMMENTS ADDED');
-            res.redirect('/');
+  newComment.save()
+    .then((data) => {
+      console.log('COMMENTS ADDED');
+      res.redirect('/');
 
-          }).catch((err) => console.log(err));
-})
+    }).catch((err) => console.log(err));
+});
 
 module.exports = router;
