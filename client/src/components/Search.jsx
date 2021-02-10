7d3cb05008fb34494874ae9f6b6cf896420df0d3
Patrice W <phwill2015@gmail.com>
2021-02-10 12:35:13 -0600

    styled navigation bar

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,31 @@
+// import React, {Component} from 'react.js';
+// import SearchBar from 'material-ui/icons/search-bar';
+// import axios from 'axios';
+
+// class Search extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       inputValue: '',
+//     };
+//   }
+
+//   componentDidMount() {
+
+//   }
+
+//   handleSubmit () {
+
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <SearchBar/>;
+//       </div>
+//     );
+
+//   }
+
+// }
+
+// export default Search;
