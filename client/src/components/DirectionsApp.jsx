0220c861f7797e9fda2993ea8acff2fbb2f5be98
Ian <iancoder@1097@gmail.com>
2021-02-18 21:18:23 -0600

    ADD/ map directions functional, dynamically choose cities

--- client/src/components/DirectionsApp.jsx
+++ client/src/components/DirectionsApp.jsx
@@ -10,7 +10,7 @@ const App2 = () => {
 
     <div className="App2">
       <MapLoader
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyApJprtU8Vs2A4LnROUFoL7aEq8qMEeOGE"
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBNnvDK_pdAFPnpRVDTmch3T0uCiDGMW2E"
         loadingElement={<div style={{ height: '100%' }} />}
       />
     </div>
